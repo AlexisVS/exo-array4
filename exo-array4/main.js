55227ba55bf4e8a7717fb3153bcd09113e078475
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-16 15:18:48 +0200

    Add files via upload

---
+++ exo-array4/main.js
@@ -0,0 +1,7 @@
+let arr = ["coding 11", "coding 12", "coding 13"];
+console.log(arr);
+arr.push("Alexis");
+arr.splice(0,1,"Plus de coding 11");
+console.log(arr);
+arr.splice(1,1);
+console.log(arr)
